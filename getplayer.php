d637afc5560fc66ec54508e7981c650d18f9b283
Reline <nathanielreline@gmail.com>
2015-11-06 17:03:58 -0500

    Changed search query

--- getplayer.php
+++ getplayer.php
@@ -5,7 +5,7 @@
 	
 	connectToDB();
 
-	$safe_player_id = mysqli_real_escape_string($mysqli, $_GET['Name']);
+	$safe_player_id = mysqli_real_escape_string($mysqli, $_GET['GoogleID']);
 
 	$get_player_query = "SELECT ID, Name, Latitude, Longitude, GoogleID FROM Player WHERE GoogleID = " . $safe_player_id;
 	$get_player_response = mysqli_query($mysqli, $get_player_query) or die(mysqli_error($mysqli));
