91cbbf9ebcedfaac5f1606d558a15b49577b80bb
Reline <nathanielreline@gmail.com>
2016-01-24 21:32:53 -0500

    Tagging works

--- tag.php
+++ tag.php
@@ -11,10 +11,14 @@
 		$safe_game_id = mysqli_real_escape_string($mysqli, $_POST['GameID']);
 		$safe_date = mysqli_real_escape_string($mysqli, $_POST['Date']);
 
-		$query = "UPDATE Player_Game SET It = '0' WHERE PlayerGoogleID = '" . $safe_old_id . "' AND GameID = '" . $safe_game_id . "'; UPDATE Player_Game SET It = '1', LastTagged = '" . $safe_date . "' WHERE PlayerGoogleID = '" . $safe_new_id . "' AND GameID = '" . $safe_game_id . "';";
-		$response = mysqli_query($mysqli, $query) or die(mysqli_error($mysqli));
-		
-		mysqli_free_result($response);
+		$query1 = "UPDATE Player_Game SET It = '0' WHERE PlayerGoogleID = '" . $safe_old_id . "' AND GameID = '" . $safe_game_id . "'";
+		$response1 = mysqli_query($mysqli, $query1) or die(mysqli_error($mysqli));
+
+		$query2 = "UPDATE Player_Game SET It = '1', LastTagged = '" . $safe_date . "' WHERE PlayerGoogleID = '" . $safe_new_id . "' AND GameID = '" . $safe_game_id . "'";
+		$response2 = mysqli_query($mysqli, $query2) or die(mysqli_error($mysqli));
+
+		mysqli_free_result($response1);
+		mysqli_free_result($response2);
 		mysqli_close($mysqli);
 	}
 ?>
