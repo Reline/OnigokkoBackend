39dab072454d3561172f69ad6e2e439058c68ad2
Reline <nathanielreline@gmail.com>
2016-03-31 19:57:21 -0400

    Delete game script finished

--- deletegame.php
+++ deletegame.php
@@ -14,12 +14,18 @@ include 'common.php';
         
         //get input & clean them
         $safe_id = mysqli_real_escape_string($mysqli, $_POST['ID']);
-           
-        // time to d-d-d-...d-d-d-d-delete
+       
+	// delete players from bridge table
+	$delete_players_query = "DELETE FROM Player_Game WHERE GameID = " . $safe_id;
+	$delete_players_response = mysqli_query($mysqli, $delete_players_query) or die(mysqli_error($mysqli));
+
+	mysqli_free_result($delete_players_response);
+ 
+	// delete game   
         $delete_game_query = "DELETE FROM Game WHERE ID = " . $safe_id;
         $delete_game_response = mysqli_query($mysqli, $delete_game_query) or die(mysqli_error($mysqli));
 
-        mysqli_free_result($edit_game_response);
+        mysqli_free_result($delete_game_response);
         mysqli_close($mysqli);
     }
 ?>
