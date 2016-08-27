871e81e7ce3a92e3bf6bdb1bbd615ef1a8194928
Reline <nathanielreline@gmail.com>
2016-08-27 13:38:37 -0400

    Add player

--- src/xyz/projectplay/onigokko/player.go
+++ src/xyz/projectplay/onigokko/player.go
@@ -3,79 +3,89 @@ package main
 import (
 	"net/http"
 	"fmt"
-	//"log"
 	"encoding/json"
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
 )
 
-var db *sql.DB
+type player struct {
+	id	  string	`json:"id"`
+	name      string	`json:"name"`
+	latitude  float64	`json:"latitude"`
+	longitude float64	`json:"longitude"`
+}
 
-func oauth(w http.ResponseWriter, req *http.Request) {
+/**
+Takes Name and Google ID,
+Determines whether to create or update player
+*/
+func oauth(w http.ResponseWriter, r *http.Request) {
 	// todo: if player does not exist within the database, create it using params
-	nate := Player{Name: "Nathan Reline"}
+	//p := player{id:r.Form.Get("id"), name:r.Form.Get("name")}
+
+	id := r.FormValue("id")
+	p := queryPlayer(id)
+
+	if (p == player{}) {
+		n := r.FormValue("name")
+		fmt.Print(n)
+		insertPlayer(id, n)
+	}
+}
+
+/**
+Takes Name and Google ID
+Creates a player in the database using Google ID as identifier
+ */
+func addPlayer(w http.ResponseWriter, r *http.Request) {
+
+}
+
+/**
+Takes either Name, Lat & Lng, or both, with a Google ID in all cases
+Updates player
+ */
+func updatePlayer(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func getPlayer(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
-	if err := json.NewEncoder(w).Encode(nate); err != nil {
+	p := queryPlayer(r.FormValue("id"))
+	if err := json.NewEncoder(w).Encode(p); err != nil {
 		panic(err) // todo: don't panic
 	}
 }
 
-func OpenDB() {
-	database, err := sql.Open("mysql", "root:mysql@/onigokko")
-	db = database
-	if err != nil {
-		panic(err.Error()) // todo: Just for example purpose. You should use proper error handling instead of panic
-	}
-
-	err = db.Ping()
-	if err != nil {
-		panic(err.Error()) // todo: proper error handling instead of panic in your app
-	}
-}
-
-func QueryPlayer(id string)  {
+func queryPlayer(id string) (p player){
 
 	// Prepare statement for reading data
-	stmtOut, err := db.Prepare("SELECT * FROM Player WHERE GoogleID = ?")
+	stmtOut, err := db.Prepare("SELECT * FROM Player WHERE ID = ?")
 	if err != nil {
 		panic(err.Error()) // todo: proper error handling instead of panic in your app
 	}
 	defer stmtOut.Close()
-
-	row := stmtOut.QueryRow("116912744736380502344")
-	player := Player{}
-	err = row.Scan(&player.ID, &player.GoogleID, &player.Name, &player.Latitude, &player.Longitude)
+	row := stmtOut.QueryRow(id)
+	var lat sql.NullFloat64
+	var lng sql.NullFloat64
+	err = row.Scan(&p.id, &p.name, &lat, &lng)
 	if err != nil {
-		fmt.Printf("SQL Err: %s", err)
+		fmt.Printf("Query Player SQL Err: %s\n", err)
 	}
-	fmt.Print(player)
+	if lat.Valid && lng.Valid {
+		p.latitude = lat.Float64
+		p.longitude = lng.Float64
+	}
+	return
 }
 
-func main() {
-	//http.HandleFunc("/Onigokko/player/oauth", oauth)
-	//log.Fatal(http.ListenAndServeTLS(":8443", "/etc/letsencrypt/live/projectplay.xyz/cert.pem", "/etc/letsencrypt/live/projectplay.xyz/privkey.pem", nil))
-
-	OpenDB()
-	QueryPlayer("116912744736380502344")
-
-	defer db.Close()
-}
-
-type Player struct {
-	ID		int	`json:"id"`
-	GoogleID  	string	`json:"googleid"`
-	Name      	string	`json:"name"`
-	Latitude  	float64	`json:"latitude"`
-	Longitude 	float64	`json:"longitude"`
-}
-
-type Game struct {
-	ID		int	`json:"id"`
-	Name		string	`json:"name"`
-	TagDistance	float64	`json:"tag_distance"`
-	HintDistance	float64	`json:"hint_distance"`
-	ImmunityTime	uint64	`json:"immunity_time"`
-	OwnerID		string	`json:"owner_id"`
-	OniID		string	`json:"oni_id"`
+func insertPlayer(id string, n string) {
+	// todo: use proper syntax
+	_, err := db.Exec(
+		"INSERT INTO Player (ID, Name) VALUES ('" + id + "','" + n + "')",
+	)
+	if err != nil {
+		fmt.Printf("Insert Player SQL Err: %s\n", err)
+	}
 }
