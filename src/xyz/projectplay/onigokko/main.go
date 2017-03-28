ccc22c4d610a814cc11d92dc12531375879fcc4a
Reline <nathanielreline@gmail.com>
2017-03-27 22:12:44 -0400

    Verify Google API token

--- src/xyz/projectplay/onigokko/main.go
+++ src/xyz/projectplay/onigokko/main.go
@@ -5,6 +5,7 @@ import "log"
 import "net/http"
 import "github.com/julienschmidt/httprouter"
 import "xyz/projectplay/onigokko/controllers"
+import "google.golang.org/api/oauth2/v2"
 
 func main() {
 	r := httprouter.New()
@@ -26,8 +27,23 @@ func hello(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	if token != "" {
-		fmt.Fprint(w, "I got your token, it was "+token+"\n")
+		tokenInfo, err := getTokenInfo(token)
+		if err != nil {
+			fmt.Fprint(w, err)
+		} else {
+			fmt.Fprint(w, tokenInfo)
+		}
 	} else {
 		fmt.Fprint(w, "You didn't send me a token...\n")
 	}
+}
+
+func getTokenInfo(idToken string) (*oauth2.Tokeninfo, error) {
+	oauth2Service, err := oauth2.New(&http.Client{})
+	if err != nil {
+		return nil, err
+	}
+	tokenInfoCall := oauth2Service.Tokeninfo()
+	tokenInfoCall.IdToken(idToken)
+	return tokenInfoCall.Do()
 }
